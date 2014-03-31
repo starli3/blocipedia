976c2d9382e2839d224a48a39847624e9d75b004
Star Li <star.estella.li@gmail.com>
2014-03-30 18:55:31 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blocipedia::Application.config.secret_token = '2bdd2ed99a8a5d0744cf819aafc8dbaf16863b2cb6033ea8eab5caf6ff37c1c9b9d7297870024807d23c34b63969cc802acf826ebe4b9429c639d6b60e5181f4'
