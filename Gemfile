afc8e065e2dfd8920929d40bff0813af2928d5e2
Star Li <star.estella.li@gmail.com>
2014-04-01 01:56:06 -0700

    Created Wiki resource and updated index and show views

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'bootstrap-sass', '~> 3.1.1'
 
 gem 'devise'
 
+gem 'faker'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Blocipedia::Application.routes.draw do
+  
+  resources :wiki
+
   devise_for :users
 
-  get "welcome/index"
-
-  get "welcome/about"
+  get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
 
