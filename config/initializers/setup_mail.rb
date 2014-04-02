0df0cac19e507c0d6b5e9fad9374face5364940d
Star Li <star.estella.li@gmail.com>
2014-04-02 09:25:35 -0700

    Added setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      "app23573803@heroku.com",
-    password:       "le1v2txt",
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
